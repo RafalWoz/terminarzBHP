0ea65fad43d0a07c7cec3521de06b3acdac524e4
RafalWoz <rwozniakpipes@.com>
2026-04-16 12:03:08 +0200

    fix: final attempt at GUS XML structure (no prefixes)

--- public/regon_proxy.php
+++ public/regon_proxy.php
@@ -7,12 +7,13 @@ $nip = $_GET['nip'] ?? '';
 $key = 'b8abef9133434c1a90c3';
 $url = 'https://wyszukiwarkaregon.stat.gov.pl/wsBIR/UslugaBIRzewnPubl.svc';
 
-function callGus($url, $action, $body, $sid = null) {
+function callGus($url, $action, $body) {
+    global $sid;
     $envelope = <<<XML
-<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:ns="http://CIS/BIR/PUBL/2014/07">
-    <soap:Header xmlns:wsa="http://www.w3.org/2005/08/addressing">
-        <wsa:Action>$action</wsa:Action>
-        <wsa:To>$url</wsa:To>
+<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:a="http://www.w3.org/2005/08/addressing">
+    <soap:Header>
+        <a:Action soap:mustUnderstand="1">$action</a:Action>
+        <a:To soap:mustUnderstand="1">$url</a:To>
     </soap:Header>
     <soap:Body>$body</soap:Body>
 </soap:Envelope>
@@ -23,7 +24,7 @@ XML;
         "Content-Type: application/soap+xml; charset=utf-8; action=\"$action\"",
         'Content-Length: ' . strlen($envelope)
     ];
-    if ($sid) {
+    if (isset($sid)) {
         $headers[] = "sid: $sid";
     }
 
@@ -40,48 +41,42 @@ XML;
 }
 
 $log = "--- START LOG " . date('Y-m-d H:i:s') . " ---\n";
-$log .= "NIP: $nip\n";
 
 // 1. LOGIN
 $loginAction = 'http://CIS/BIR/PUBL/2014/07/IUslugaBIRzewnPubl/Zaloguj';
-$loginBody = "<ns:Zaloguj><ns:pKluczUzytkownika>$key</ns:pKluczUzytkownika></ns:Zaloguj>";
+$loginBody = '<Zaloguj xmlns="http://CIS/BIR/PUBL/2014/07"><pKluczUzytkownika>' . $key . '</pKluczUzytkownika></Zaloguj>';
 $loginResp = callGus($url, $loginAction, $loginBody);
-
-$log .= "LOGIN RESPONSE: $loginResp\n";
 
 if (preg_match('/<ZalogujResult[^>]*>(.*)<\/ZalogujResult>/', $loginResp, $matches)) {
     $sid = $matches[1];
-    $log .= "SID: $sid\n";
     
     // 2. SEARCH
     $searchAction = 'http://CIS/BIR/PUBL/2014/07/IUslugaBIRzewnPubl/DaneSzukajPodmioty';
+    // Kluczowa zmiana: używamy domyślnego namespace bez prefiksów ns/dat
     $searchBody = <<<XML
-<ns:DaneSzukajPodmioty>
-    <ns:pParametryWyszukiwania>
-        <dat:Nip xmlns:dat="http://CIS/BIR/PUBL/2014/07/datacontract">$nip</dat:Nip>
-    </ns:pParametryWyszukiwania>
-</ns:DaneSzukajPodmioty>
+<DaneSzukajPodmioty xmlns="http://CIS/BIR/PUBL/2014/07">
+    <pParametryWyszukiwania>
+        <Nip>$nip</Nip>
+    </pParametryWyszukiwania>
+</DaneSzukajPodmioty>
 XML;
 
-    $searchResp = callGus($url, $searchAction, $searchBody, $sid);
+    $searchResp = callGus($url, $searchAction, $searchBody);
     $log .= "SEARCH RESPONSE: $searchResp\n";
     
-    if (preg_match('/<DaneSzukajPodmiotyResult[^>]*>(.*)<\/DaneSzukajPodmiotyResult>/s', $searchResp, $matches)) {
+    if (preg_match('/<DaneSzukajPodmiotyResult[^>]*>(.*)<\/DaneSzukajPodmiotyResult>/s', $searchResp, $matches) && !empty($matches[1])) {
         $xmlData = html_entity_decode($matches[1]);
         $xml = @simplexml_load_string($xmlData);
         if ($xml && $xml->dane) {
              $d = $xml->dane;
-             echo json_encode(['success' => true, 'data' => ['name' => (string)$d->Nazwa]]);
-        } else {
-             echo json_encode(['error' => 'No data found in XML', 'xml' => $xmlData]);
+             echo json_encode(['success' => true, 'data' => [
+                 'name' => (string)$d->Nazwa,
+                 'address' => trim((string)$d->Ulica . ' ' . (string)$d->NrNieruchomosci . ((string)$d->NrLokalu ? '/'.(string)$d->NrLokalu : '') . ', ' . (string)$d->KodPocztowy . ' ' . (string)$d->Miejscowosc)
+             ]]);
+             exit;
         }
-    } else {
-        echo json_encode(['error' => 'No DaneSzukajPodmiotyResult found']);
     }
-
-} else {
-    echo json_encode(['error' => 'Login failed']);
 }
 
 file_put_contents('gus_debug.txt', $log, FILE_APPEND);
-// echo "\n--- LOG SAVED TO gus_debug.txt ---"; 
+echo json_encode(['error' => 'GUS nie zwrócił danych dla tego NIP.', 'debug_hint' => 'Sprawdź logi na serwerze.']);
