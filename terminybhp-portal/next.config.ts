bad2bb7bedd1aada79d4cff663a3c57028381132
RafalWoz <rwozniakpipes@.com>
2026-05-14 12:55:43 +0200

    Configure subfolder routing for /serwis and Next.js proxy

--- terminybhp-portal/next.config.ts
+++ terminybhp-portal/next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/serwis/:path*',
+        // Adres LH.pl. Zależnie od struktury konta, może wymagać usunięcia "/terminybhp"
+        destination: 'https://serwer50635.lh.pl/terminybhp/serwis/:path*',
+      },
+      {
+        source: '/serwis',
+        destination: 'https://serwer50635.lh.pl/terminybhp/serwis/index.html',
+      }
+    ];
+  },
 };
 
 export default nextConfig;
