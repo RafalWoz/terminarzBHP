2dd7d9edea6e25247d8e6341ab4d528955dadb0f
RafalWoz <59577177+RafalWoz@users.noreply.github.com>
2026-06-02 09:30:34 +0200

    Add legacy root service worker cleanup script

---
+++ terminybhp-portal/public/sw.js
@@ -0,0 +1,15 @@
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    self.registration.unregister().then(() =>
+      self.clients.matchAll({ type: "window" }).then((clients) => {
+        for (const client of clients) {
+          client.navigate(client.url);
+        }
+      }),
+    ),
+  );
+});
