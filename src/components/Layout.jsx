c2f0bc73ef5d74dd728a3681aec6782f26eb2759
RafalWoz <59577177+RafalWoz@users.noreply.github.com>
2026-06-18 11:45:04 +0200

    Show contextual breadcrumbs in app layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,19 @@
 import { Outlet } from 'react-router-dom';
 import TopBar from './TopBar';
 import BottomNav from './BottomNav';
+import ContextBreadcrumbs from './ContextBreadcrumbs';
 
 export default function Layout() {
   return (
     <div className="flex flex-col h-full bg-gray-50">
       <TopBar />
-      <main className="flex-1 overflow-y-auto pb-16 lg:pb-8">
-        <Outlet />
-      </main>
+      <div className="flex-1 min-h-0 lg:flex">
+        <ContextBreadcrumbs />
+        <main className="flex-1 overflow-y-auto pb-16 lg:pb-8">
+          <ContextBreadcrumbs variant="mobile" />
+          <Outlet />
+        </main>
+      </div>
       <BottomNav />
     </div>
   );
